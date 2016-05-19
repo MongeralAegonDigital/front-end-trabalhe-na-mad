c017e37cdabe771bdf7af31283623c442544eaf8
wlsdesign <wlsdesign@gmail.com>
2016-05-19 01:21:47 -0300

    Add packetJson e Gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+
+/* Converter SASS em CSS */
+gulp.task('sass', function(){
+  return gulp.src('static/sass/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('static/css'))
+});
+
+/* Minifica o CSS */
+gulp.task('minify-css', function() {
+  return gulp.src('static/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('static/css'));
+});
+
+/* Minifica o JS */
+gulp.task('minify-js', function() {
+  return gulp.src('static/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('static/js'));
+});
+
+/* Minifica o HTML */
+gulp.task('minify-html', function() {
+  return gulp.src('./*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./'))
+});
+
+/* Verificando arquivos */
+gulp.task('watch', function(){
+  gulp.watch('static/sass/**/*.scss', ['sass']);
+  gulp.watch('static/js/**/*.js', ['minify-js']);
+  gulp.watch('./*.html', ['minify-html']);
+});
