8d6910c063062832c885780fb6d30b76fad138c8
henriqueviana <henriquehvisa@gmail.com>
2016-12-24 13:05:18 -0200

    Criado o arquivo de inicialização da aplicação e o componente principal

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainComponent from './components/MainComponent';
+
+const app = document.querySelector('.app');
+
+ReactDOM.render(<MainComponent/> , app);
---
+++ app/components/MainComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainComponent extends React.Component{
+	render(){
+		return(
+			<section>
+				<p>Teste</p>
+			</section>
+		)
+	}
+}
+
+export default MainComponent;
